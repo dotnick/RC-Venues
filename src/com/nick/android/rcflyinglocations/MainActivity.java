5e79f14413cef0897f4b53de291351bf55e8ac07
Nick Nikolaou <nikolasnikolaou1@gmail.com>
2012-05-03 22:41:36 +0100

    ActionbarSherlock and Dashboard (temp icons)

--- src/com/nick/android/rcflyinglocations/MainActivity.java
+++ src/com/nick/android/rcflyinglocations/MainActivity.java
@@ -1,43 +1,15 @@
 package com.nick.android.rcflyinglocations;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.ImageView;
 
-public class MainActivity extends Activity {
-	
-	private ImageView button1;
-	private ImageView button2;
-	
+import com.actionbarsherlock.app.SherlockActivity;
+
+public class MainActivity extends SherlockActivity {
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main_menu);
-		
-		button1 = (ImageView) findViewById(R.id.button1);
-		button1.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				Intent toMap = new Intent(MainActivity.this, MapsActivity.class);
-				startActivity(toMap);
-			}
-			
-		});
-		
-		button2 = (ImageView) findViewById(R.id.button2);
-		button2.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				Intent toSearch = new Intent(MainActivity.this, SearchActivity.class);
-				startActivity(toSearch);
-			}
-			
-		});
+		setContentView(R.layout.fragment_layout);
 		
 		
 	}
